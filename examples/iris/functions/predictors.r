4c41dc19b32ed667cb2ab5002e544bcd0436915f
Bill West <williamjwest@gmail.com>
2014-01-07 22:55:22 -0500

    Adjusted commenting in iris example.

--- examples/iris/functions/predictors.r
+++ examples/iris/functions/predictors.r
@@ -4,10 +4,10 @@
 #### TODO: create generic for predict.haystack
 
 predict.haystack.randomforest<-function(x,model){
+    #      get list of predictions of the Haystack
+    #      object dataset using randomforest predictive model
 
-    "      get list of predictions of the Haystack object dataset using randomforest predictive model
-    "
-  dev_mean<-.5 # mean of binary target in training data
+    dev_mean<-.5 # mean of binary target in training data
   val_mean<-.5 # mean of binary target in validation data (representative sample)
   pred <- predict(model,newdata=x$data,type="prob")[,"1"]
   pred_adj_odds <- pred/(1-pred)*val_mean/(1-val_mean)*(1-dev_mean)/dev_mean
@@ -16,8 +16,8 @@ predict.haystack.randomforest<-function(x,model){
   return(pred_adj)
 }
 predict.haystack.glm<-function(x,model){
-    "      get list of predictions of the Haystack object dataset using Generalized linear model predictive model
-    "
+    #      get list of predictions of the Haystack object
+    #      dataset using Generalized linear model predictive model
 
   dev_mean<-.5 # mean of binary target in training data
   val_mean<-.5 # mean of binary target in validation data (representative sample)
